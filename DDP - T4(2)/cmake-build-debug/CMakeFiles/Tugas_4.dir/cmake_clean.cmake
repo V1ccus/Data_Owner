ef7aa1e4efac097c7107dac344a07de02edfd5f3
Hosea Nicolas Andre Nahar Pratama Putra <672021159@student.uksw.edu>
2022-02-14 17:14:51 +0700

    Data Owner
    DDP | 672021159

---
+++ DDP - T4(2)/cmake-build-debug/CMakeFiles/Tugas_4.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tugas_4.dir/main.cpp.obj"
+  "CMakeFiles/Tugas_4.dir/main.cpp.obj.d"
+  "Tugas_4.exe"
+  "Tugas_4.exe.manifest"
+  "Tugas_4.pdb"
+  "libTugas_4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tugas_4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ DDP - T4(2)/cmake-build-debug/CMakeFiles/Tugas_4.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for Tugas_4.
+# This may be replaced when dependencies are built.
